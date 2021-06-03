952575322a73840b7ac7b41f8f37fe2fe531b093
Oleg Pudeyev <code@olegp.name>
2021-06-02 23:09:38 -0400

    fix installer

--- mongo/devuan/install.sh
+++ mongo/devuan/install.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-rsync -av "$(dirname "$0")" /usr/local/m/versions --exclude install.sh
+if test "`id -u`" != 0; then
+  if which sudo 2>/dev/null 1>&2; then
+    echo "This needs to be run as root, trying sudo" 1>&2
+    exec sudo sh -x "$0" "$@"
+  else
+    echo "This needs to be run as root" 1>&2
+    exit 2
+  fi
+fi
+
+mkdir -p /usr/local/m/versions
+
+rsync -av "$(dirname "$0")"/ /usr/local/m/versions --exclude install.sh
